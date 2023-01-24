5267e4ec83efdc03496d4e85836d4e8d0baebf0a
Berkay Tuncel <berkaytncl98@gmail.com>
2023-01-25 00:01:05 +0300

    Add endpoints

--- AnimePicker/Utilities/APIURLs.swift
+++ AnimePicker/Utilities/APIURLs.swift
@@ -10,6 +10,21 @@ import Foundation
 enum APIURLs {
     private static let baseURL = "https://api.jikan.moe/v4"
     
-    static let topAnimes = "\(baseURL)/top/anime"
     static let randomAnime = "\(baseURL)/random/anime"
+    
+    static func topAnimes(page: Int) -> String {
+        "\(baseURL)/top/anime?page=\(page)"
+    }
+    
+    static func selectedAnime(id: Int) -> String {
+        "\(baseURL)/anime/\(id)"
+    }
+    
+    
+    
+    static let recommendations = "\(baseURL)/recommendations/anime"
+    
+    static func topCharacters(page: Int) -> String {
+        "\(baseURL)/top/characters?page=\(page)"
+    }
 }
